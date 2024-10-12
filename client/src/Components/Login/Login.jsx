29c83d040c4644b84b2e6c2f1ae2435e41034b97
cePORRAZar <116750047+PorasZarec@users.noreply.github.com>
2024-10-13 03:37:49 +0800

    Change of mind

---
+++ client/src/Components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div>
+        This is Login page
+        <a href="/register">To Login</a>
+        <a href="/dashboard">To Login</a>
+    </div>
+  )
+}
+
+export default Login
