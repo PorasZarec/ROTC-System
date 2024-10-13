b4b36ccb52a854f9b5418969da8d7b18d6b0f400
cePORRAZar <116750047+PorasZarec@users.noreply.github.com>
2024-10-14 01:34:37 +0800

    Login and Signup Page

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,14 +1,79 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import "../../App.css";
+import { Link } from "react-router-dom";
+
+// Import assets
+import video from "../../LoginAssets/video.mp4";
+import logo from "../../LoginAssets/ROTC_Logo.png";
+
+// Imported icons
+import { FaUserShield } from "react-icons/fa";
+import { BsShieldLockFill } from "react-icons/bs";
+import { AiOutlineSwapRight } from "react-icons/ai";
 
 const Login = () => {
   return (
-    <div>
-        This is Login page
-        <a href="/register">To Login</a>
-        <a href="/dashboard">To Login</a>
-    </div>
-  )
-}
+    <div className="loginPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
 
-export default Login
+          <div className="textDiv">
+            <h2 className="title">Create And Sell Extra Ordinary Products</h2>
+            <p>Adopt the peace of nature!</p>
+          </div>
+
+          <div className="footerDiv flex">
+            <span className="text">Don't have and account?</span>
+            <Link to={"/register"}>
+              <button className="btn">Sign Up</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="formDiv flex">
+          <div className="headerDiv">
+            <img src={logo} alt="Logo Image" />
+            <h3>Welcome Back!</h3>
+          </div>
+
+          <form action="" className="form grid">
+            <span className="showMessage">Login status will go here</span>
+
+            <div className="inputDiv">
+              <label htmlFor="username">Username</label>
+              <div className="input flex">
+                <FaUserShield className="icon" />
+                <input type="text" id="username" placeholder="Enter Username" />
+              </div>
+            </div>
+
+            <div className="inputDiv">
+              <label htmlFor="password">Password</label>
+              <div className="input flex">
+                <BsShieldLockFill className="icon" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter Password"
+                />
+              </div>
+            </div>
+
+            <button type="submit" className="btn flex">
+              <span>Login</span>
+              <AiOutlineSwapRight className="icon" />
+            </button>
+
+            <span className="forgotPassword">
+              Forgot your password? <a href="">Click here</a>
+            </span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -1,13 +1,89 @@
-import React from 'react'
-import './Register.css'
+import React from "react";
+import "./Register.css";
+import "../../App.css";
+import { Link } from "react-router-dom";
+
+// Import assets
+import video from "../../LoginAssets/video.mp4";
+import logo from "../../LoginAssets/ROTC_Logo.png";
+
+// Imported icons
+import { FaUserShield } from "react-icons/fa";
+import { BsShieldLockFill } from "react-icons/bs";
+import { AiOutlineSwapRight } from "react-icons/ai";
+import { MdMarkEmailRead } from "react-icons/md";
 
 const Register = () => {
   return (
-    <div>
-        This is Register Page
-        <a href="/login">To Login</a>
-    </div>
-  )
-}
+    <div className="registerPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
 
-export default Register
+          <div className="textDiv">
+            <h2 className="title">Create And Sell Extra Ordinary Products</h2>
+            <p>Adopt the peace of nature!</p>
+          </div>
+
+          <div className="footerDiv flex">
+            <span className="text">Have and account?</span>
+            <Link to={"/"}>
+              <button className="btn">Login</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="formDiv flex">
+          <div className="headerDiv">
+            <img src={logo} alt="Logo Image" />
+            <h3>Let Us Know You!</h3>
+          </div>
+
+          <form action="" className="form grid">
+
+            <div className="inputDiv">
+              <label htmlFor="email">Email</label>
+              <div className="input flex">
+                <MdMarkEmailRead className="icon" />
+                <input type="email" id="email" placeholder="Enter Email" />
+              </div>
+            </div>
+
+
+            <div className="inputDiv">
+              <label htmlFor="username">Username</label>
+              <div className="input flex">
+                <FaUserShield className="icon" />
+                <input type="text" id="username" placeholder="Enter Username" />
+              </div>
+            </div>
+
+
+            <div className="inputDiv">
+              <label htmlFor="password">Password</label>
+              <div className="input flex">
+                <BsShieldLockFill className="icon" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter Password"
+                />
+              </div>
+            </div>
+
+            <button type="submit" className="btn flex">
+              <span>Register</span>
+              <AiOutlineSwapRight className="icon" />
+            </button>
+
+            <span className="forgotPassword">
+              Forgot your password? <a href="">Click here</a>
+            </span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
