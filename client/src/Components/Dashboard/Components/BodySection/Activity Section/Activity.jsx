1b9fdd81fa2afa493d4812a41cddc518522d9a96
cePORRAZar <116750047+PorasZarec@users.noreply.github.com>
2024-10-19 02:25:37 +0800

    Activity Panel added

--- client/src/Components/Dashboard/Components/BodySection/Activity Section/Activity.jsx
+++ client/src/Components/Dashboard/Components/BodySection/Activity Section/Activity.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 import './activity.css'
 
+// imported Images
+
+import user from "../../../../Assets/user (3).jpg";
+
+
+// imported Icons
+
+import { BsArrowRightShort } from "react-icons/bs";
+
 const Activity = () => {
   return (
-    <div>Activity</div>
+    <div className='activitySection'>
+        <div className="heading flex">
+            <h1>Recent Activity</h1>
+            <button className='btn flex'>
+                See All
+                <BsArrowRightShort className='icon'/>
+            </button>
+        </div>
+
+        <div className="secContainer grid">
+
+            <div className="singleCustomer flex">
+                <img src={user} alt="Customer Image" />
+                <div className="customerDetails">
+                    <span className="name">Ola Mamamiya</span>
+                    <small>Ordered new plant</small>
+                </div>
+                <div className="duration">
+                    2 min ago
+                </div>
+            </div>
+
+            <div className="singleCustomer flex">
+                <img src={user} alt="Customer Image" />
+                <div className="customerDetails">
+                    <span className="name">Ola Mamamiya</span>
+                    <small>Ordered new plant</small>
+                </div>
+                <div className="duration">
+                    2 min ago
+                </div>
+            </div>
+
+            <div className="singleCustomer flex">
+                <img src={user} alt="Customer Image" />
+                <div className="customerDetails">
+                    <span className="name">Ola Mamamiya</span>
+                    <small>Ordered new plant</small>
+                </div>
+                <div className="duration">
+                    2 min ago
+                </div>
+            </div>
+
+            <div className="singleCustomer flex">
+                <img src={user} alt="Customer Image" />
+                <div className="customerDetails">
+                    <span className="name">Ola Mamamiya</span>
+                    <small>Ordered new plant</small>
+                </div>
+                <div className="duration">
+                    2 min ago
+                </div>
+            </div>
+
+            <div className="singleCustomer flex">
+                <img src={user} alt="Customer Image" />
+                <div className="customerDetails">
+                    <span className="name">Ola Mamamiya</span>
+                    <small>Ordered new plant</small>
+                </div>
+                <div className="duration">
+                    2 min ago
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
